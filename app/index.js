c4c5881ef59dc0a4bc5e37f8c26c05d8ffb69d6c
Lin Zhang <lin.zhang.ux@gmail.com>
2020-09-10 13:04:27 -0700

    import Popular component

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
+import Popular from './components/Popular';
 
 //component
 //state
@@ -9,7 +10,10 @@ import './index.css'
 class App extends Component{
     render(){
         return(
-            <div>Hello world ✨🖐🐶</div>
+            // <div>Hello world ✨🖐🐶</div>
+            <div className="container">
+                <Popular />
+            </div>
         ) 
     }
 }
