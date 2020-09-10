b1cefe86861e0c7489545c20efac0c4aa6d3481c
Lin Zhang <lin.zhang.ux@gmail.com>
2020-09-10 16:08:03 -0700

    add click handler and style for selected language link

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,14 +1,31 @@
 import React from 'react'
 
 class Popular extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            selectedLanguage: 'All'
+        }
+
+        this.updateSelectedLanguage = this.updateSelectedLanguage.bind(this);
+    }
+
+    updateSelectedLanguage(selectedLanguage){
+        this.setState({
+            selectedLanguage
+        })
+    }
     render(){
         const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
         return(
         <ul className='flex-center'>
             {
                 languages.map((language, index) => (
-                <li key={index}>
-                    <button className='btn-clear nav-link'>
+                <li key={language}>
+                    <button className='btn-clear nav-link'
+                        style={ language === this.state.selectedLanguage ? { color: 'rgb(59, 7, 150)' } : null }
+                        onClick={() => this.updateSelectedLanguage(language)}
+                    >
                         {language}
                     </button>
                 </li>
