0229dd93846a53b5f6ddcc7b6322b0ac40ad1ce3
Lin Zhang <lin.zhang.ux@gmail.com>
2020-09-10 21:20:05 -0700

    validate prop types for LanguageNav

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,4 +1,30 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
+function  LanguageNav({selectedLanguage, updateSelectedLanguage}){
+    const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
+    return(
+        <ul className='flex-center'>
+            {
+                languages.map((language) => (
+                    <li key={language}>
+                        <button className='btn-clear nav-link'
+                            style={ language === selectedLanguage ? { color: 'rgb(59, 7, 150)' } : null }
+                            onClick={() => updateSelectedLanguage(language)}
+                        >
+                            {language}
+                        </button>
+                    </li>
+                    ))
+            }
+        </ul>
+    );
+}
+
+LanguageNav.propTypes = {
+    selectedLanguage: PropTypes.string.isRequired,
+    updateSelectedLanguage: PropTypes.func.isRequired,
+}
 
 class Popular extends React.Component{
     constructor(props){
@@ -15,23 +41,14 @@ class Popular extends React.Component{
             selectedLanguage
         })
     }
+
     render(){
-        const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
         return(
-        <ul className='flex-center'>
-            {
-                languages.map((language, index) => (
-                <li key={language}>
-                    <button className='btn-clear nav-link'
-                        style={ language === this.state.selectedLanguage ? { color: 'rgb(59, 7, 150)' } : null }
-                        onClick={() => this.updateSelectedLanguage(language)}
-                    >
-                        {language}
-                    </button>
-                </li>
-                ))
-            }
-        </ul>)
+            <LanguageNav
+                selectedLanguage = {this.state.selectedLanguage}
+                updateSelectedLanguage = {this.updateSelectedLanguage}
+            ></LanguageNav>
+        )
     }
 }
 
