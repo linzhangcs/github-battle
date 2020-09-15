c245dcb7d4da7b2584dbe8bee27c32d87fb64570
Lin Zhang <lin.zhang.ux@gmail.com>
2020-09-14 19:14:55 -0700

    create Card component and pass into children

---
+++ app/components/Card.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Card({ header, subheader, avatar, href, name, children }){
+    return(
+        <div className="card bg-light">
+            <h4 className="header-lg center-text">
+                {header}
+            </h4>
+            <img
+                className='avatar'
+                src={avatar}
+                alt={`avatar for ${name}`} />
+
+           {subheader && (<h4 className='center-text'>
+                {subheader}
+            </h4>)} 
+            <h2 className='center-text'>
+                <a href={href} className='link'>
+                    {name}
+                </a>
+            </h2>
+            {/* render the children JSX */}
+            {children}
+        </div>
+    )
+}
+Card.propTypes = {
+    header: PropTypes.string.isRequired,
+    subheader: PropTypes.string,
+    avatar: PropTypes.string.isRequired,
+    href: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+}
+export default Card;
