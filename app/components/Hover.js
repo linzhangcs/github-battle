0c83adfe78f3dcb6bdd41569c71f6c60a7da7df4
Lin Zhang <lin.zhang.ux@gmail.com>
2020-09-18 19:12:40 -0700

    create Hover rendering Prop

---
+++ app/components/Hover.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+class Hover extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            hovering: false,
+        }
+
+        this.mouseOver = this.mouseOver.bind(this);
+        this.mouseOut = this.mouseOut.bind(this);
+    }
+    mouseOver(){
+        this.setState({
+            hovering: true
+        })
+    }
+    mouseOut(){
+        this.setState({
+            hovering: false
+        })
+    }
+
+    render(){
+        return(
+            <div onMouseOut={this.mouseOut} onMouseOver={this.mouseOver}>
+                {this.props.children(this.state.hovering)}
+            </div>
+        )
+    }
+}
+
+export default Hover;
