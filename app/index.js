d236f59fc23fa94c394b41ef8624c280abe35283
Lin Zhang <lin.zhang.ux@gmail.com>
2020-09-19 23:57:21 -0700

    update App state for theme and toggleTheme - pass theme state using ThemeProvider

--- app/index.js
+++ app/index.js
@@ -1,20 +1,43 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'
+import './index.css';
 import Popular from './components/Popular';
-import Battle from './components/Battle'
+import Battle from './components/Battle';
+import { ThemeProvider } from './contexts/theme';
+import Nav from './components/Nav';
+
 //component
 //state
 //lifecycle
 //ui render
 class App extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            theme: "light",
+            toggleTheme: () => {
+                this.setState(({ theme }) => ({
+                    theme: theme === 'light' ? 'dark' : 'light',
+                }))
+            }
+        }
+    }
+    
     render(){
         return(
             // <div>Hello world ✨🖐🐶</div>
-            <div className="container">
-                {/* <Popular /> */}
-                <Battle />
-            </div>
+            // <Battle />
+            // <Popular />
+         /* <Popular /> */
+                <ThemeProvider value={this.state}>
+                <div className={this.state.theme}>
+                    <div className="container">
+                        <Nav />
+                        <Battle />
+                    </div>
+                </div>
+                </ThemeProvider>
         ) 
     }
 }
