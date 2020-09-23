f902e43349dc31546bf5f58a5909540822a9acb2
Lin Zhang <lin.zhang.ux@gmail.com>
2020-09-22 23:21:21 -0700

    import BrowserRouter to the index and direct routes to popular and battle components

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ import Popular from './components/Popular';
 import Battle from './components/Battle';
 import { ThemeProvider } from './contexts/theme';
 import Nav from './components/Nav';
-
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 //component
 //state
 //lifecycle
@@ -30,15 +30,19 @@ class App extends Component{
             // <Battle />
             // <Popular />
          /* <Popular /> */
+            <Router >
                 <ThemeProvider value={this.state}>
-                <div className={this.state.theme}>
-                    <div className="container">
-                        <Nav />
-                        {/* <Battle /> */}
-                        <Popular />
+                    <div className={this.state.theme}>
+                        <div className="container">
+                            <Nav />
+                            {/* <Battle /> */}
+                            {/* <Popular /> */}
+                            <Route exact path='/' component={Popular} />
+                            <Route path='/battle' component={Battle} />
+                        </div>
                     </div>
-                </div>
                 </ThemeProvider>
+            </Router>
         ) 
     }
 }
