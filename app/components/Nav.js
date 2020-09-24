fa821338208ef42352e5345ff76e69c2ccc7c3e1
Lin Zhang <lin.zhang.ux@gmail.com>
2020-09-23 23:42:03 -0700

    include NavLink to nav component for adding links to / and /battle path, also used activeStyle for active link

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { ThemeConsumer } from '../contexts/theme';
+import { NavLink } from 'react-router-dom';
+import { FaRegBell } from 'react-icons/fa';
 
+const activeStyle ={
+    color: 'rgb(100, 100, 234)'
+}
 function Nav(){
     return(
         <ThemeConsumer>
             {({theme, toggleTheme}) => (
-                <nav className = 'row space-between'>
+                <nav className = 'row space-btw'>
+                    <ul className='row nav'>
+                        <li><NavLink className='nav-link' exact activeStyle={activeStyle} to='/'>Popular</NavLink></li>
+                        <li><NavLink className='nav-link' activeStyle={activeStyle} to='/battle'>Battle</NavLink></li>
+                    </ul>
                     <button
                      style={{fontSize: 30}}
                      className="btn-clear"
